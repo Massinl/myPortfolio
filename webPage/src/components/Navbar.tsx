f7f12cb0cb812fb79fc164cf9906c636f01a0a9b
massinl <cristian.massini@upr.edu>
2025-10-30 15:26:13 -0400

    fixed nav Bar! now stretches completely

--- webPage/src/components/Navbar.tsx
+++ webPage/src/components/Navbar.tsx
@@ -1,14 +1,31 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
+  const location = useLocation();
+
   return (
-    <nav>
-      <Link to="/">Home</Link> | 
-      <Link to="/projects">Projects</Link> | 
-      <Link to="/resume">Resume</Link> | 
-      <Link to="/contact">Contact</Link>
+    <nav className="navbar">
+      <h2 className="navbar-logo">Cristian Massini</h2>
+
+      <div className="navbar-links">
+        <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+          Home
+        </Link>
+        <Link to="/about" className={location.pathname === "/about" ? "active" : ""}>
+          About
+        </Link>
+        <Link to="/beats" className={location.pathname === "/beats" ? "active" : ""}>
+          My Beats
+        </Link>
+        <Link to="/resume" className={location.pathname === "/resume" ? "active" : ""}>
+          Resume
+        </Link>
+        <Link to="/contact" className={location.pathname === "/contact" ? "active" : ""}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 }
